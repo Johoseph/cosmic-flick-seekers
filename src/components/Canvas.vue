11295e3862ac6218ce3a9da9b2ffb76d8478760f
Joseph Moroney <josephmoroney97@gmail.com>
2021-11-27 13:18:59 +1000

    More styling

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,8 +1,10 @@
 <template>
+  <component :is="'style'" type="text/css">
+    .img-move { top: {{ imgTop }}px; left: {{ imgLeft }}px; }
+  </component>
   <div class="w-screen h-screen overflow-hidden relative">
     <img
-      class="absolute object-cover object-center mw-auto mh-auto img-custom"
-      v-on:mousemove="handleMouseMove($event)"
+      class="absolute object-cover object-center mw-auto mh-auto img-custom img-move"
       src="./../assets/background.jpg"
       role="presentation"
     />
@@ -14,11 +16,20 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Canvas",
+  data: () => ({
+    imgTop: -40,
+    imgLeft: -40,
+  }),
   methods: {
     handleMouseMove(e) {
       console.log(e);
-      console.log(this);
     },
+  },
+  beforeMount() {
+    window.addEventListener("mousemove", this.handleMouseMove);
+  },
+  beforeUnmount() {
+    window.removeEventListener("mousemove", this.handleMouseMove);
   },
 });
 </script>
@@ -27,8 +38,6 @@ export default defineComponent({
 .img-custom {
   width: calc(100vw + 80px);
   height: calc(100vh + 80px);
-  top: -40px;
-  left: -40px;
   max-width: unset;
 }
 </style>
