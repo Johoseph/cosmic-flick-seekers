5df2ddb02fefedcaa269bcbab3858cffb39c10f6
Joseph Moroney <josephmoroney97@gmail.com>
2021-11-27 18:24:29 +1000

    Updated background movement logic

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,43 +1,84 @@
 <template>
   <div
-    class="div-custom img-move w-screen h-screen overflow-hidden fixed"
+    class="background img-move w-screen h-screen overflow-hidden fixed"
     :style="{
       backgroundPositionX: `${imgLeft}px`,
       backgroundPositionY: `${imgTop}px`,
+      backgroundSize: `${
+        autoWidth ? `auto calc(100vh + 80px)` : `calc(100vw + 80px) auto`
+      }`,
     }"
   />
+  <!-- <div class="temp fixed w-screen h-screen" /> -->
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
-const IMG_PADDING = 40;
+const image = {
+  ratio: 16 / 9,
+  width: 1920,
+  height: 1080,
+  denominator: 9,
+  padding: 40, // Pixels hidden off page
+};
+
+const getAutoWidth = () =>
+  image.ratio > document.body.clientWidth / document.body.clientHeight;
 
 export default defineComponent({
   name: "Canvas",
   data: () => ({
-    imgTop: -IMG_PADDING,
-    imgLeft: -IMG_PADDING,
+    imgTop: -image.padding,
+    imgLeft: -image.padding,
+    autoWidth: true,
   }),
   methods: {
     handleMouseMove(e) {
-      this.imgTop = -IMG_PADDING + e.clientY / (IMG_PADDING * 2);
-      this.imgLeft = -IMG_PADDING + e.clientX / (IMG_PADDING * 2);
+      let horizontalPosition = image.padding;
+
+      // Calculate how much width is being 'cut off' by screen
+      if (this.autoWidth) {
+        const screenResolution =
+          document.body.clientWidth / document.body.clientHeight;
+
+        const actualImgWidth =
+          (image.height / image.denominator) *
+          (screenResolution * image.denominator);
+        horizontalPosition = (image.width - actualImgWidth) / 2;
+      }
+
+      this.imgTop = -image.padding + e.clientY / (image.padding * 2);
+      this.imgLeft = -horizontalPosition + e.clientX / (image.padding * 2);
+    },
+    handleResize() {
+      this.autoWidth = getAutoWidth();
     },
   },
   beforeMount() {
     window.addEventListener("mousemove", this.handleMouseMove);
   },
+  mounted() {
+    this.autoWidth = getAutoWidth();
+    window.addEventListener("resize", this.handleResize);
+  },
   beforeUnmount() {
     window.removeEventListener("mousemove", this.handleMouseMove);
+    window.removeEventListener("resize", this.handleResize);
   },
 });
 </script>
 
 <style scoped>
-.div-custom {
+.background {
   background: url("./../assets/background.jpg");
   background-repeat: no-repeat;
-  background-size: calc(100vw + 80px) calc(100vh + 80px);
+}
+
+.temp {
+  background: url("./../assets/temp-for-placement.png");
+  background-repeat: no-repeat;
+  background-size: auto calc(100vh + 80px);
+  background-position: center;
 }
 </style>
