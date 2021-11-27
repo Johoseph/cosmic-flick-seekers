5f287cde129bda545223a3fe848152b2f0233466
Joseph Moroney <josephmoroney97@gmail.com>
2021-11-27 23:50:12 +1000

    Manually managing hover transition

--- src/components/Letters.vue
+++ src/components/Letters.vue
@@ -6,7 +6,10 @@
       top: `${config.top}`,
       left: `${config.left}`,
       background: `url(${getImgUrl(config.file)})`,
+      transform: `scale(${scale})`,
     }"
+    v-on:mouseover="this.handleHover"
+    v-on:mouseleave="this.handleLeave"
   />
 </template>
 
@@ -15,11 +18,22 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Canvas",
+  data: () => ({
+    scale: 1,
+  }),
   props: ["config"],
   methods: {
     getImgUrl(letter) {
       const images = require.context("./../assets/text/", false, /\.svg$/);
       return images(`./${letter}`);
+    },
+    handleHover(e) {
+      const letterPos = e.target.getBoundingClientRect();
+
+      if (e.clientY - letterPos.y <= 115) this.scale = 1.1;
+    },
+    handleLeave() {
+      this.scale = 1;
     },
   },
 });
@@ -28,13 +42,8 @@ export default defineComponent({
 <style scoped>
 .static-letter {
   /* display: none; */
-  height: 999px;
+  height: 700px;
   background-repeat: no-repeat !important;
-  transform: scale(1);
   transition: transform 800ms cubic-bezier(0.17, 0.67, 0.64, 0.92);
-}
-
-.static-letter:hover {
-  transform: scale(1.1);
 }
 </style>
